e874b27d6103fd286a05ce30cb68dcd26a800d59
David Mihal <david@dmihal.com>
2023-12-29 10:37:37 +0800

    Predicate allows basic transfer

--- gas_predicate/src/main.sw
+++ gas_predicate/src/main.sw
@@ -2,33 +2,100 @@ predicate;
 
 use std::{
     constants::ZERO_B256,
-    tx::tx_script_bytecode_hash,
     b512::B512,
     hash::{sha256, Hash},
+    inputs::{
+        Input,
+        input_asset_id,
+        input_count,
+        input_owner,
+        input_type,
+    },
     tx::{
         tx_id,
         tx_witness_data,
+        tx_script_bytecode_hash,
+        tx_script_length,
+        tx_witnesses_count,
     },
     ecr::ec_recover_address,
 };
 
 configurable {
     SIGNER: Address = Address::from(ZERO_B256),
+    NFT_CONTRACT_ID: ContractId = ContractId::from(ZERO_B256),
     EXPECTED_SCRIPT_BYTECODE_HASH: b256 = ZERO_B256,
 }
 
-fn main() -> bool {
-    let script_bytecode_hash: b256 = tx_script_bytecode_hash();
-    if (script_bytecode_hash != EXPECTED_SCRIPT_BYTECODE_HASH) {
-        return false;
-    }
+const GTF_INPUT_CONTRACT_CONTRACT_ID = 0x113;
 
-    let signature: B512 = tx_witness_data(0);
+
+fn main() -> bool {
+    let signature: B512 = tx_witness_data(tx_witnesses_count() - 1);
 
     let signer_address = ec_recover_address(signature, sha256(tx_id())).unwrap();
     if (signer_address != SIGNER) {
         return false;
     }
 
+    let is_script_valid = if (tx_script_length() > 0) {
+        let script_bytecode_hash: b256 = tx_script_bytecode_hash();
+        script_bytecode_hash == EXPECTED_SCRIPT_BYTECODE_HASH
+    } else { true };
+    if (!is_script_valid) {
+        return false;
+    }
+
+    let predicate_addr = predicate_address();
+    if (NFT_CONTRACT_ID == ContractId::from(ZERO_B256)) {
+        return false;
+    }
+
+    let num_inputs = input_count().as_u64();
+    let mut i = 0;
+    while i < num_inputs {
+        match input_type(i) {
+            Input::Coin => {
+                let asset_id = input_asset_id(i).unwrap();
+                if (asset_id == AssetId::from(ZERO_B256)) {
+                    let owner = input_owner(i).unwrap();
+                    if (owner != predicate_addr) {
+                        return false;
+                    }
+                }
+            },
+            Input::Message => {
+                revert(0);
+            },
+            Input::Contract => {
+                let contract_id = input_contract_id(i).unwrap();
+                if (contract_id != NFT_CONTRACT_ID) {
+                    return false;
+                }
+            },
+        }
+        i = i + 1;
+    }
+
     return true;
+}
+
+fn predicate_address() -> Address {
+    let predicate_index = asm(r1) {
+        gm r1 i3;
+        r1: u64
+    };
+    input_owner(predicate_index).unwrap()
+}
+
+
+fn input_contract_id(index: u64) -> Option<ContractId> {
+    match input_type(index) {
+        Input::Contract => {
+            let addr_ptr = __gtf::<raw_ptr>(index, GTF_INPUT_CONTRACT_CONTRACT_ID);
+            // Why do I have to add 2?
+            Some(addr_ptr.add::<u64>(2).read::<ContractId>())
+        },
+        _ => None,
+    }
 }
--- tests/harness.rs
+++ tests/harness.rs
@@ -62,9 +62,10 @@ async fn get_script<T: Account>(account: T, nft: ContractId) -> (NFTScript<T>, B
     (script, b256)
 }
 
-fn get_predicate(script_hash: Bits256, signer: Address, provider: &Provider) -> Predicate {
+fn get_predicate(script_hash: Bits256, signer: Address, nft_contract_id: ContractId, provider: &Provider) -> Predicate {
     let configurables = GasPredicateConfigurables::new()
         .with_SIGNER(signer)
+        .with_NFT_CONTRACT_ID(nft_contract_id)
         .with_EXPECTED_SCRIPT_BYTECODE_HASH(script_hash);
 
     let mut predicate: Predicate = Predicate::load_from("./gas_predicate/out/debug/gas_predicate.bin")
@@ -80,12 +81,18 @@ async fn can_use_script() {
     let wallets = get_wallets().await;
     let deployer = &wallets[0];
     let user = &wallets[1];
+    let user_2 = &wallets[2];
     let fuel_provider = deployer.provider().unwrap();
     let network_info = fuel_provider.network_info().await.unwrap();
 
     let nft_instance = get_contract_instance(deployer).await;
     let (_script, script_hash) = get_script(user.clone(), nft_instance.id().into()).await;
-    let predicate = get_predicate(script_hash, deployer.address().into(), deployer.provider().unwrap());
+    let predicate = get_predicate(
+        script_hash,
+        deployer.address().into(),
+        nft_instance.id().into(),
+        deployer.provider().unwrap(),
+    );
 
     deployer.transfer(predicate.address(), 10000, BASE_ASSET_ID, TxParameters::default())
         .await
@@ -155,40 +162,74 @@ async fn can_use_script() {
     let tx_status = fuel_provider.tx_status(&actual_tx_id).await.unwrap();
     let receipts = tx_status.take_receipts_checked(None).unwrap();
 
+    let mut nft_asset_id: Option<AssetId> = None;
+    let mut nft_sub_id: Option<Bytes32> = None;
     for item in receipts.iter() {
         match item {
-            Receipt::Mint{ contract_id, .. } => {
+            Receipt::Mint{ contract_id, sub_id, .. } => {
                 let nft_id: ContractId = nft_instance.id().into();
                 assert!(contract_id.clone() == nft_id);
+                nft_sub_id = Some(*sub_id);
             },
-            Receipt::TransferOut{ to, .. } => {
+            Receipt::TransferOut{ to, asset_id, .. } => {
                 let user_address: Address = user.address().into();
                 assert!(to.clone() == user_address);
+                nft_asset_id = Some(*asset_id);
             },
             _ => {},
         }
     }
+
+    // =================
+    // Do basic transfer
+    // =================
+
+    let mut nft_inputs = user
+        .get_asset_inputs_for_amount(nft_asset_id.unwrap(), 1)
+        .await
+        .unwrap();
+
+    let eth_inputs = predicate
+        .get_asset_inputs_for_amount(BASE_ASSET_ID, 1000)
+        .await
+        .unwrap();
+    nft_inputs.extend(eth_inputs);
+
+    let outputs = vec![
+        Output::Change {
+            to: predicate.address().into(),
+            amount: 0,
+            asset_id: BASE_ASSET_ID,
+        },
+        Output::Coin {
+            to: user_2.address().into(),
+            amount: 1,
+            asset_id: nft_asset_id.unwrap(),
+        },
+    ];
+
+    // Create the Tx
+    let mut transaction_builder = ScriptTransactionBuilder::prepare_transfer(
+        nft_inputs,
+        outputs,
+        TxParameters::default(),
+        network_info.clone(),
+    );
+
+    user.sign_transaction(&mut transaction_builder);
+
+    let mut script_transaction = transaction_builder.build().unwrap();
+
+    let expected_tx_id = script_transaction.id(network_info.chain_id());
+    let signature = deployer.sign_message(expected_tx_id).await.unwrap();
+    script_transaction.append_witness(signature.as_ref().into());
+
+    let actual_tx_id = fuel_provider.send_transaction_and_await_commit(script_transaction).await.unwrap();
+    assert_eq!(expected_tx_id, actual_tx_id);
+
+    let tx_status = fuel_provider.tx_status(&actual_tx_id).await.unwrap();
+    let receipts = tx_status.take_receipts_checked(None).unwrap();
 }
-
-// #[tokio::test]
-// async fn cant_do_transfer() {
-//     let wallets = get_wallets().await;
-//     let deployer = &wallets[0];
-//     let user = &wallets[1];
-
-//     let contract_instance = get_contract_instance(deployer).await;
-//     let (script, script_hash) = get_script(user.clone(), contract_instance.id().into()).await;
-//     let predicate = get_predicate(script_hash, deployer.provider().unwrap());
-
-//     deployer.transfer(predicate.address(), 10000, BASE_ASSET_ID, TxParameters::default())
-//         .await
-//         .unwrap();
-
-//     let is_err = predicate.transfer(user.address(), 10000, BASE_ASSET_ID, TxParameters::default())
-//         .await
-//         .is_err();
-//     assert!(is_err);
-// }
 
 fn vec_to_str(vec: &Vec<u8>) -> String {
     vec.iter().map(|b| format!("{:02x}", b)).collect()
